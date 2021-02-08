fad4bcaf1cef926180c87e4ae719da84d119e24a
Kyle Burns <kyle@kburns.us>
2021-02-08 05:27:29 -0500

    changes default port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
-var port = process.env.port || 1337;
+var port = process.env.port || 8080;
 http.createServer(function(request, response){
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end({'now':'foo'}); // we'll be replacing this later
+    response.end('Response from NodeJS Web App'); // we'll be replacing this later
 }).listen(port);
