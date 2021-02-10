948ceb5634a216add476bc7e8d755bc8cdf74026
Kyle Burns <kyle@kburns.us>
2021-02-10 02:28:03 -1000

    adds documentation to build script

--- infra/build.sh
+++ infra/build.sh
@@ -4,6 +4,12 @@
 # SCRIPTS, IT IS NOT INTENDED FOR ANY PURPOSE OTHER THAN DEMONSTRATION
 # IN ITS CURRENT FORM
 #
+
+# 
+# build the npm package and zip the deployable application artifacts
+# for use in the deployment. The folder "/build/Release" (relative to repo root)
+# is included in the .gitignore so that build outputs will not be put in the repo
+#
 npm install ..
 mkdir -p ../build/Release
 zip -r ../build/Release/269.zip ../node_modules ../package-lock.json ../package.json ../server.js
